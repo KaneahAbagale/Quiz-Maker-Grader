5c707db253fb5cdc533ec1eceb7d804632726681
Gertrude Abagale <74657401+KaneahAbagale@users.noreply.github.com>
2025-04-25 00:19:15 +0000

    Delete username_validator.py

--- username_validator.py
+++
@@ -1,9 +0,0 @@
-# Create a variable to store Username
-
-user_name = input("Please eneter your user name\n")
-
-#check if user name meets criteria
-if len(user_name) >= 10 and user_name[0].isalpha and user_name.isalnum():
-    print("Congratulations your user name is valid")
-else:
-    print("Hey your user name is not valid")
